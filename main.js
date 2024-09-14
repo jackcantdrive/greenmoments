cb00126ec674196d39e03c230dc2ab4c0d99f178
x <>
2024-09-14 23:55:55 +0800

    cleanup

--- main.js
+++ main.js
@@ -1,49 +1,4 @@
-// async function startCamera() {
-//     try {
-//         // Get access to the user's media devices
-//         const stream = await navigator.mediaDevices.getUserMedia({
-//             video: {
-//                 facingMode: 'environment'
-//             }
-//         });
-
-//         const video = document.getElementById('video');
-//         const canvas = document.getElementById('canvas');
-//         const context = canvas.getContext('2d');
-
-//         video.srcObject = stream;
-
-//         video.onloadedmetadata = () => {
-//             canvas.width = video.videoWidth;
-//             canvas.height = video.videoHeight;
-
-//             const draw = () => {
-//                 context.drawImage(video, 0, 0, canvas.width, canvas.height);
-//                 // requestAnimationFrame(draw);
-//             };
-//             draw();
-//             setInterval(draw, 1000/60)
-//         };
-//     } catch (error) {
-//         console.error('Error accessing camera:', error);
-//     }
-// }
-
-
-// // window.onload = startCamera;
-
-
-// alert(1)
-
 async function startCamera() {
-    // try {
-    // Get access to the user's media devices
-    // const stream = await navigator.mediaDevices.getUserMedia({
-    //     video: {
-    //         facingMode: 'environment'
-    //     }
-    // });
-
     const stream = await navigator.mediaDevices.getUserMedia({
         video: {
             facingMode: 'environment'
@@ -54,19 +9,10 @@ async function startCamera() {
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
 
+    // Black Magic to get ios webcam to work
     video.setAttribute('autoplay', '');
     video.setAttribute('muted', '');
     video.setAttribute('playsinline', '')
-
-    // video.srcObject = stream;
-
-    // const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-
-    // const stream = await navigator.mediaDevices.getUserMedia({
-    //     video: {
-    //         facingMode: 'environment'
-    //     }
-    // });
 
     video.srcObject = stream;
 
@@ -79,12 +25,5 @@ async function startCamera() {
             requestAnimationFrame(draw);
         };
         draw();
-        // setInterval(draw, 1000/60)
     };
-    // } catch (error) {
-    //     console.error('Error accessing camera:', error);
-    // }
 }
-
-
-// window.onload = startCamera;
